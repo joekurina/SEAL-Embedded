dd04255eb966b70b1b2edf45cbc34e9cbcec1b3c
Joe Kurina <jkurina86@proton.me>
2025-03-01 20:08:12 -0800

    deleted some stuff

--- device/lib/lib_fft_rtl.hpp
+++
@@ -1,6 +0,0 @@
-#include <sycl/sycl.hpp>
-#include "complex.h"
-
-using fft_complex = double _Complex;
-
-SYCL_EXTERNAL extern "C" fft_complex rtl_fft( fft_input);
--- device/lib/lib_ifft_rtl.hpp
+++
@@ -1,4 +0,0 @@
-#include <sycl/sycl.hpp>
-#include <complex>
-
-SYCL_EXTERNAL extern "C" std::complex<double> rtl_ifft(std::complex<double> ifft_input);
