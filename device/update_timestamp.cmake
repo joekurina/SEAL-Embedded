c6c0864de0e3f9f99446d5d6d2d9a0461077043a
Joe Kurina <jkurina86@proton.me>
2025-03-03 07:38:01 -0800

    feat: add CMakeLists.txt for SEAL-Embedded project and remove outdated timestamp script

--- device/update_timestamp.cmake
+++
@@ -1,4 +0,0 @@
-# Helper script to update timestamp in index.html
-file(READ "/home/joe/Projects/SEAL-Embedded/device/build/reports/index.html.in" index_content)
-string(REPLACE "@TIMESTAMP@" "${TIMESTAMP}" updated_content "${index_content}")
-file(WRITE "/home/joe/Projects/SEAL-Embedded/device/build/reports/index.html" "${updated_content}")
