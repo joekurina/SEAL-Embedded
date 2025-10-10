19424fd68ff63819777ae2454b6f85af9bb8bc09
Joe Kurina <jkurina86@proton.me>
2025-10-09 20:56:41 -0700

    Removed the old NTT from the includes and updated the CMakeLists.txt file to include the RTL NTT files for report generation.

--- device/lib/SYCL_ckks_sym.cpp
+++ device/lib/SYCL_ckks_sym.cpp
@@ -7,8 +7,8 @@
 
 // Include the SYCL kernel headers
 #include "SYCL_ifft.h"
-#include "SYCL_ntt_a.h"
-#include "SYCL_ntt_b.h"
+//#include "SYCL_ntt_a.h"
+//#include "SYCL_ntt_b.h"
 #include "SYCL_poly_mult_neg.h"
 #include "SYCL_poly_add.h"
 #include "SYCL_scale_and_reduce.h"
@@ -28,8 +28,8 @@ using namespace sycl;
 
 // Forward declare kernel names
 class IFFTKernel;
-class NTTKernel_A;
-class NTTKernel_B;
+//class NTTKernel_A;
+//class NTTKernel_B;
 class RTLNTTKernel_A_Input;
 class RTLNTTKernel_A;
 class RTLNTTKernel_A_Output;
